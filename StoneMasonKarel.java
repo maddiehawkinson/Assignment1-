136e97f0aaa1f98ae3b2be36f1e381195d26560e
S <s>
2015-01-08 23:49:16 -0800

    {"type":"TI", "runs":"1"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -7,7 +7,9 @@ public class StoneMasonKarel extends SuperKarel {
 				move();
 				fixArch();	
 			}
-			turnAround(); 
+			turnLeft(); 
+			move();
+			turnLeft();
 		}
 	private void fixArch() {
 		if (noBeepersPresent()) {
