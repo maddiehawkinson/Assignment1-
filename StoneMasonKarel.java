98ab9d11156e29f387bb24c7e337e5b2085bed95
S <s>
2015-01-08 23:59:11 -0800

    {"type":"TI", "runs":"1"}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -4,8 +4,11 @@ import stanford.karel.*;
 public class StoneMasonKarel extends SuperKarel {
 	public void run () {
 		while (frontIsClear()) {
+			putBeeper();
 			move();
-			putBeeper();	
+				for (int i=0; i<4; i++) {
+					putBeeper();
+				}
 		}
 			turnLeft(); 
 			move();
